1fa3670c85fdee51e8b944e1ebe21509ec421b68
Loc <locle2001@gmail.com>
2021-12-25 01:28:57 +0700

    reDeploy

--- src/index.js
+++ src/index.js
@@ -15,7 +15,14 @@ db.connect();
 //name
 const app = express();
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+    origin: "https://xenodochial-brahmagupta-6b5e18.netlify.app/",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+};
+// app.use(cors());
+app.use(cors(corsOptions));
+
 //Routes
 const route = require("./routes");
 route(app);
