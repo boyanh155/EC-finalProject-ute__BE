e7a4da36bdc3a3666e1db5e9dafba6883a4fd617
Loc <locle2001@gmail.com>
2021-12-25 13:44:39 +0700

    redeploy

--- src/index.js
+++ src/index.js
@@ -20,8 +20,8 @@ const corsOptions = {
     credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200,
 };
-// app.use(cors());
-app.use(cors(corsOptions));
+app.use(cors());
+// app.use(cors(corsOptions));
 
 //Routes
 const route = require("./routes");
